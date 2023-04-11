2505a0e19d8c40a3014da5e13503e3de29be22dc
betogroo <luizhumberto@gmail.com>
2023-04-11 20:02:48 -0300

    method example

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ const vm = Vue.createApp({
       lastName: "Groo",
     };
   },
+  methods: {
+    fullName() {
+      return `${this.firstName} ${this.lastName}`;
+    },
+  },
 }).mount("#app");
 
 /* setTimeout(() => {
