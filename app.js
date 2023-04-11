ecd0784973316f3384a6f0c5fcc7ddfef191f9d6
betogroo <luizhumberto@gmail.com>
2023-04-11 14:24:58 -0300

    multiple instances example

--- app.js
+++ app.js
@@ -6,3 +6,12 @@ Vue.createApp({
     };
   },
 }).mount("#app");
+/* Vue.createApp({
+  data() {
+    return {
+      firstName: "Beto",
+      lastName: "Groo",
+    };
+  },
+}).mount("#app1");
+ */
