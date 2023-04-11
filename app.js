d20a10c9e6592422038c4116d39564efaeb5d74c
betogroo <luizhumberto@gmail.com>
2023-04-11 14:38:49 -0300

    access instance data example

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-Vue.createApp({
+const vm = Vue.createApp({
   data() {
     return {
       firstName: "Beto",
@@ -6,6 +6,10 @@ Vue.createApp({
     };
   },
 }).mount("#app");
+
+/* setTimeout(() => {
+  vm.lastName = "Garcia";
+}, 2000); */
 /* Vue.createApp({
   data() {
     return {
