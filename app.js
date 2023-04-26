5b0e3b59694abe09627a10ba7e7bda5f00cb0074
betogroo <luizhumberto@gmail.com>
2023-04-26 15:03:05 -0300

    lesson 43 end

--- app.js
+++ app.js
@@ -1,37 +1,22 @@
 let vm = Vue.createApp({
   data() {
     return {
-      message: "Hello world!"
-    }
+      message: "Hello world!",
+    };
   },
-  beforeCreate() {
-    console.log('beforeCreate() function called!', this.message)
-  },
-  created() {
-    console.log('created() function called!', this.message)
-  },
-  beforeMount() {
-    console.log('beforeMount() function called!', this.$el)
-  },
-  mounted() {
-    console.log('mounted() function called!', this.$el)
-  },
-  beforeUpdate() {
-    console.log('beforeUpdate() function called!')
-  },
-  updated() {
-    console.log('updated() funcction called!')
-  },
-  beforeUnmount() {
-    console.log('beforeUnmount() function called!')
-  },
-  unmounted() {
-    console.log('unmounted() function called!')
-  }
-})
+  //template: `{{message}}`,
+});
 
-vm.mount('#app')
+vm.mount("#app");
 
-// setTimeout(() => {
-//   vm.mount('#app')
-// }, 3000)
+/* let vm2 = Vue.createApp({
+  data() {
+    return {
+      message: "Hello world2!",
+    };
+  },
+  render() {
+    return Vue.h("h1", this.message);
+  },
+}).mount("#app2");
+ */
